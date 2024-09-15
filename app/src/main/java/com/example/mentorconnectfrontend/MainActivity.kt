9ff68300e07449eadfbfe92ed1b7a15d123ebae7
Parimal <parimalmourya03@gmail.com>
2024-09-15 14:18:21 +0530

    OTP Final 2

--- app/src/main/java/com/example/mentorconnectfrontend/MainActivity.kt
+++ app/src/main/java/com/example/mentorconnectfrontend/MainActivity.kt
@@ -3,29 +3,13 @@ package com.example.mentorconnectfrontend
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.mentorconnectfrontend.ui.theme.MentorConnectFrontEndTheme
 
 class MainActivity : ComponentActivity() {
@@ -61,58 +45,4 @@ val WorkSans = FontFamily(
 
 
 
-@Preview(showBackground = true)
-@Composable
-fun LoadingScreen() {
-    Box(modifier = Modifier
-        .fillMaxSize()
-        .background(
-            brush = Brush.verticalGradient(
-                colors = listOf(Color(0xFF235391), Color(0xFF2675B9), Color(0xFF2BAFFC))
-            )
-        ))
 
-    Column (
-        modifier = Modifier
-            .fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ){
-        Text(
-            text = stringResource(R.string.application_name),
-            color = Color.White,
-            style = TextStyle(
-                fontFamily = Cabin,
-                fontSize = 48.sp,
-                fontWeight = FontWeight.SemiBold
-            )
-        )
-        Text(
-            text = stringResource(R.string.tagline),
-            color = Color.White,
-            style = TextStyle(
-                fontSize = 20.sp,
-                fontWeight = FontWeight.Medium,
-                fontFamily = WorkSans
-            )
-        )
-    }
-
-    Row(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(bottom = 44.dp),
-        verticalAlignment = Alignment.Bottom,
-        horizontalArrangement = Arrangement.Center
-    ) {
-        Text(
-            text = stringResource(R.string.team_name),
-            color = Color.White,
-            style = TextStyle(
-                fontSize = 24.sp,
-                fontWeight = FontWeight.Medium,
-                fontFamily = WorkSans
-            )
-        )
-    }
-}
